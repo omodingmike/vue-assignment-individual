b3973fcbfb77ecad4414c9ba78d1a27776afabde
MikeOmoding <omodingmike@gmail.com>
2023-04-18 10:08:38 +0300

    refactored code

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const {createApp} = Vue
 createApp({
-    methods:{
-        toggleBulbState(){
+    methods: {
+        toggleBulbState() {
             this.bulbOn = !this.bulbOn;
+            this.imageSource = this.bulbOn ? 'assets/images/on.png' : 'assets/images/off.png'
         }
     },
-    data(){
-        return{
-            bulbOn:false
+    data() {
+        return {
+            bulbOn: false,
+            imageSource: 'assets/images/off.png'
         }
     }
 }).mount('#app')
