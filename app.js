dbc945008a973389b047f3f70b87410099087a51
MikeOmoding <omodingmike@gmail.com>
2023-04-18 17:52:30 +0300

    added tv assigment

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ createApp({
     methods: {
         toggleBulbState() {
             this.bulbOn = !this.bulbOn;
-            this.imageSource = this.bulbOn ? 'assets/images/on.png' : 'assets/images/off.png'
+            this.imageSource = this.bulbOn ? 'assets/images/on.png' : 'assets/images/off.png';
+            this.bulbButtonText = this.bulbOn ? "TURN OFF" : "TURN ON"
+        },
+        toggleTvState() {
+            this.tvOn = !this.tvOn;
         }
     },
     data() {
         return {
             bulbOn: false,
-            imageSource: 'assets/images/off.png'
+            imageSource: 'assets/images/off.png',
+            bulbButtonText: 'TURN ON',
+            tvOn:false,
+            showImage:false
         }
     }
 }).mount('#app')
